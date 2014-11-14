c745283c247da005aa5d28bba3b3396dfb7d7e37
zhangjunah <zhangjun19870402@126.com>
2014-11-14 23:18:58 +0800

    'add_animate'

--- index.js
+++ index.js
@@ -15,52 +15,53 @@ function renderPerformance() {
             $('.waterfall .totalTime').html(speed.total + ' ms');
 
             var cur_width = 0;
+            var animateTime = 500;
 
             // dns查找
             var dns = calc(speed.dns, speed.total);
-            $('.waterfall .total .dns').css('width', dns + '%');
+            $('.waterfall .total .dns').animate({width: dns + '%'}, animateTime);
             $('.waterfall .only .dnsTime').html(speed.dns + ' ms');
-            $('.waterfall .only .dns').css('width', dns + '%');
+            $('.waterfall .only .dns').animate({width: dns + '%'}, animateTime);
             cur_width += dns;
 
             // 建立链接
             var ct = calc(speed.ct, speed.total);
-            $('.waterfall .total .ct').css('width', ct + '%');
+            $('.waterfall .total .ct').animate({width: ct + '%'}, animateTime);
             $('.waterfall .only .ctTime').html(speed.ct + ' ms');
-            $('.waterfall .only .ct').css('width', ct + '%');
             $('.waterfall .only .ct').css('margin-left', cur_width + '%');
+            $('.waterfall .only .ct').animate({width: ct + '%'}, animateTime);
             cur_width += ct;
 
             // 首字节
             var st = calc(speed.st, speed.total);
-            $('.waterfall .total .st').css('width', st + '%');
+            $('.waterfall .total .st').animate({width: st + '%'}, animateTime);
             $('.waterfall .only .stTime').html(speed.st + ' ms');
-            $('.waterfall .only .st').css('width', st + '%');
             $('.waterfall .only .st').css('margin-left', cur_width + '%');
+            $('.waterfall .only .st').animate({width: st + '%'}, animateTime);
             cur_width += st;
 
             // html传输
             var tt = calc(speed.tt, speed.total);
-            $('.waterfall .total .tt').css('width', tt + '%');
+            $('.waterfall .total .tt').animate({width: tt + '%'}, animateTime);
             $('.waterfall .only .ttTime').html(speed.tt + ' ms');
-            $('.waterfall .only .tt').css('width', tt + '%');
             $('.waterfall .only .tt').css('margin-left', cur_width + '%');
+            $('.waterfall .only .tt').animate({width: tt + '%'}, animateTime);
             cur_width += tt;
 
             // DOM解析
             var drt = calc(speed.drt, speed.total);
-            $('.waterfall .total .drt').css('width', drt + '%');
+            $('.waterfall .total .drt').animate({width: drt + '%'}, animateTime);
             $('.waterfall .only .drtTime').html(speed.drt + ' ms');
-            $('.waterfall .only .drt').css('width', drt + '%');
             $('.waterfall .only .drt').css('margin-left', cur_width + '%');
+            $('.waterfall .only .drt').animate({width: drt + '%'}, animateTime);
             cur_width += drt;
 
             // 页面渲染
             var dct = calc(speed.dct, speed.total);
-            $('.waterfall .total .dct').css('width', dct + '%');
+            $('.waterfall .total .dct').animate({width: dct + '%'}, animateTime);
             $('.waterfall .only .dctTime').html(speed.dct + ' ms');
-            $('.waterfall .only .dct').css('width', dct + '%');
             $('.waterfall .only .dct').css('margin-left', cur_width + '%');
+            $('.waterfall .only .dct').animate({width: dct + '%'}, animateTime);
             cur_width += dct;
 
             bindEvents();
@@ -88,6 +89,6 @@ function bindEvents() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     renderPerformance();
 });
